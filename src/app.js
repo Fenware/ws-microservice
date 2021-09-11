ae316ed7d50dce2ec2a3fb5b1077636c2840480b
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-11 02:18:36 -0300

    feat: Inicializando socket.io

--- src/app.js
+++ src/app.js
@@ -3,17 +3,40 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-
+const http = require('http');
+const socketio = require('socket.io');
 require('dotenv').config();
-
-const websocketsRouter = require('./routes/wsRouter');
-
-const errorHandler = require('./middleware/errorHandler');
-const authMiddleware = require('./middleware/auth');
 
 const app = express();
 
-app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+const port =  process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+/* ---------- Inicializando Socket.io ---------- */
+const io = socketio(server, {
+  cors: {
+    /* origin: '*', */
+    origin: 'http://localhost:8081',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
+/* ---------------------------------------------- */
+
+
+/* Router y middleware */
+const websocketsRouter = require('./routes/wsRouter');
+const errorHandler = require('./middleware/errorHandler');
+const authMiddleware = require('./middleware/auth');
+
+
+/* Middlewares */
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,4 +52,14 @@ app.use((req, res, next) => {
 // pass any unhandled errors to the error handler
 app.use(errorHandler);
 
-module.exports = app;
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`); // eslint-disable-line no-console
+}
+
+server.listen(port);
+/* server.on('error', onError); */
+server.on('listening', onListening);
+
+/* module.exports = app; */
