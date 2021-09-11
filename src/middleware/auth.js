3560a959d487da7a54fee20935f53c06bc24c771
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-11 02:19:12 -0300

    refactor: Log en auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,6 @@ const axios = require('axios');
 const authMiddleware = (req, res, next) => {
   if ('authorization' in req.headers) {
     let { API_URL } = process.env;
-
     axios({
       method: 'post',
       url: `${API_URL}/token`,
@@ -14,6 +13,7 @@ const authMiddleware = (req, res, next) => {
     })
       // eslint-disable-next-line no-shadow
       .then((response) => {
+        console.log(response.data);
         if (response.data === 'OK') {
           next();
         } else {
@@ -21,7 +21,7 @@ const authMiddleware = (req, res, next) => {
         }
       })
       .catch((error) => {
-        console.log(error);
+        res.status(401).send(createError.Unauthorized());
       });
   } else {
     res.status(401).send(createError.Unauthorized());
