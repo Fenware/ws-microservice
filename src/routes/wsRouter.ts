ba5acb5c5985521d7c002e808c5ea3f65a273d7a
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-12 03:44:09 -0300

    refactor: Quitando codigo innecesario del router

--- src/routes/wsRouter.ts
+++ src/routes/wsRouter.ts
@@ -1,24 +1,15 @@
-import {Router, Request, Response} from "express";
-
-/* const SocketIoController = require('../controllers/SocketIoController'); */
-
-/* const socket = new SocketIoController(); */
+import { Router, Request, Response } from "express";
 
 const router: Router = Router();
 
-router.post('/chat-rooms', (req: Request, res: Response) => {
-  res.send({ message: 'Acá va lo de websockets para las salas de chat' });
+router.post("/chat-rooms", (req: Request, res: Response) => {
+  res.send({ message: "Acá va lo de websockets para las salas de chat" });
 });
 
-router.post('/chat-messages', (req: Request, res: Response) => {
+router.post("/chat-messages", (req: Request, res: Response) => {
   res.send({
-    message: 'Acá va lo de websockets para los mensajes de una sala de chat',
+    message: "Acá va lo de websockets para los mensajes de una sala de chat",
   });
-
- /*  const io = SocketIoController.getConnection();
-  io.on("chat:message", (data: any) => {
-    console.log(data);
-  }); */
 });
 
 export default router;
